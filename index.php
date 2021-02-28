d82eba7225aea418c7a41e9d202070c2044bc99a
Tristan Da Costa Pinto <tristandacostapinto@gmail.com>
2021-02-28 22:00:51 +0100

    setup environnement

--- index.php
+++ index.php
@@ -4,28 +4,138 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="assets/css/styles.css" rel="stylesheet">
-    <link href="assets/css/bootstrap-grid.min.css" rel="stylesheet">
+    <link href="./assets/css/styles.css" rel="stylesheet">
+    <link href="./assets/css/bootstrap.css" rel="stylesheet">
+    <link href="./assets/css/bootstrap-grid.css" rel="stylesheet">
+    <title>Elemental Idle</title>
 
 
 </head>
 
 <body>
-    <ol class="heroes">
-        <li class="water" id="1">1<span class="badge"></span>Water Mage</li>
+    <div class="container-fluid resourcesBar">
+        <div class="row">
+            <div class="resources">
+                <div class="card-group">
+                    <div class="card col-sm-2 col-lg-2 mx-2">
+                    <!-- BEGINNING CARD WATER -->
+                    <div class="card-header text-center bg-primary">Water Element</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center">WATER</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                    <!-- END CARD WATER -->
+                    <!-- BEGINNING CARD FIRE -->
+                    <div class="card col-sm-2 col-lg-2 mx-2">
+                        <div class="card-header text-center bg-danger">Fire Element</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center">FIRE</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                    <!-- END CARD FIRE -->
+                    <!-- BEGINNING CARD AIR -->
+                    <div class="card col-sm-2 col-lg-2 mx-2">
+                        <div class="card-header text-center bg-success">Air Element</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center">AIR</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                    <!-- END CARD AIR -->
+                    <!-- BEGINNING CARD EARTH -->
+                    <div class="card col-sm-2 col-lg-2 mx-2">
+                        <div class="card-header text-center bg-earth">Earth Element</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center">EARTH</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                    <!-- END CARD EARTH -->
+                    <!-- BEGINNING CARD THUNDER -->
+                    <div class="card col-sm-2 col-lg-2 mx-2">
+                        <div class="card-header text-center bg-warning">Thunder Element</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center">THUNDER</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                    <!-- END CARD THUNDER -->
+                </div>
+            </div>
+        </div>
+    </div>
 
-        <li class="water" id="2">1<span class="badge"></span>Water Mage</li>
-
-        <li class="water" id="3">1<span class="badge"></span>Water Mage</li>
-
-        <li class="water" id="4">1<span class="badge"></span>Water Mage</li>
-
-        <li class="water" id="5">1<span class="badge"></span>Water Mage</li>
-
-        <li class="water" id="6">1<span class="badge"></span>Water Mage</li>
-
-        <li class="water" id="7">1<span class="badge"></span>Water Mage</li>
-    </ol>
+    <!-- FOOTER -->
+    <div class="container-fluid fixed-bottom">
+        <div class="row">
+            <div class="resourcesFooter">
+                <div class="card-group">
+                    <div class="card col-sm-2 col-lg-2 mx-2">
+                    <!-- BEGINNING CARD WATER MAGE -->
+                    <div class="card-header text-center bg-primary">Water Mage</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center">WATER</h5>
+                            <p class="card-text">PROGRESS BAR GOES HERE</p>
+                            
+                        </div>
+                    </div>
+                    <!-- END CARD WATER -->
+                    <!-- BEGINNING CARD MAGE -->
+                    <div class="card col-sm-2 col-lg-2 mx-2">
+                        <div class="card-header text-center bg-danger">Fire Mage</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center">FIRE</h5>
+                            <p class="card-text">PROGRESS BAR GOES HERE.</p>
+                            
+                        </div>
+                    </div>
+                    <!-- END CARD FIRE -->
+                    <!-- BEGINNING CARD AIR MAGE-->
+                    <div class="card col-sm-2 col-lg-2 mx-2">
+                        <div class="card-header text-center bg-success">Air Mage</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center">AIR</h5>
+                            <p class="card-text">PROGRESS BAR GOES HERE</p>
+                            
+                        </div>
+                    </div>
+                    <!-- END CARD AIR -->
+                    <!-- BEGINNING CARD EARTH MAGE-->
+                    <div class="card col-sm-2 col-lg-2 mx-2">
+                        <div class="card-header text-center bg-earth">Earth Mage</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center">EARTH</h5>
+                            <p class="card-text">PROGRESS BAR GOES HERE</p>
+                            
+                        </div>
+                    </div>
+                    <!-- END CARD EARTH -->
+                    <!-- BEGINNING CARD THUNDER MAGE -->
+                    <div class="card col-sm-2 col-lg-2 mx-2">
+                        <div class="card-header text-center bg-warning">Thunder Mage</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center">THUNDER</h5>
+                            <p class="card-text">PROGRESS BAR GOES HERE</p>
+                            
+                        </div>
+                    </div>
+                    <!-- END CARD THUNDER -->
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 
