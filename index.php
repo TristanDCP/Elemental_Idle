817cca14f87e30a9dd6d6fe1fb6d5edc481cc6bf
Tristan Da Costa Pinto <tristandacostapinto@gmail.com>
2021-02-28 21:15:27 +0100

    Core

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/styles.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-grid.min.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+    <ol class="heroes">
+        <li class="water" id="1">1<span class="badge"></span>Water Mage</li>
+
+        <li class="water" id="2">1<span class="badge"></span>Water Mage</li>
+
+        <li class="water" id="3">1<span class="badge"></span>Water Mage</li>
+
+        <li class="water" id="4">1<span class="badge"></span>Water Mage</li>
+
+        <li class="water" id="5">1<span class="badge"></span>Water Mage</li>
+
+        <li class="water" id="6">1<span class="badge"></span>Water Mage</li>
+
+        <li class="water" id="7">1<span class="badge"></span>Water Mage</li>
+    </ol>
+
+    <script src="assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
